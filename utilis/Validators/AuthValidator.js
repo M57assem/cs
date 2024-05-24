f664fb076f86af6a9f439e2b4e0df6894267bcb4
M57assem <160439357+M57assem@users.noreply.github.com>
2024-05-24 19:31:26 +0300

    Update AuthValidator.js

--- utilis/Validators/AuthValidator.js
+++ utilis/Validators/AuthValidator.js
@@ -22,7 +22,7 @@ exports.SignupValidator = [
     check('Password')
     .notEmpty().withMessage('Password is required')
     .isLength({min:6}).withMessage('Password is too short')
-    .isLength({max:12}).withMessage('Password is too long '),
+    .isLength({max:20}).withMessage('Password is too long '),
     // .custom((password, { req }) => {
     //   if (password !== req.body.confirmPassword) {
     //     throw new Error('Password Confirmation incorrect');
