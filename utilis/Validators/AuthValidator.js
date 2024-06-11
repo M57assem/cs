f3fe10a76526053f39ad65ee65eb853629a9f142
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-11 09:48:17 +0300

    Update AuthValidator.js

--- utilis/Validators/AuthValidator.js
+++ utilis/Validators/AuthValidator.js
@@ -1,86 +1,58 @@
-const {check} = require('express-validator');
+const { check } = require('express-validator');
 const ErrorValidation = require('../../middleware/ErrorValidation');
 const ApiError = require('../utilis/handler');
 
 exports.SignupValidator = [
-    check('Name')
+  check('Name')
     .notEmpty().withMessage('Name is required')
-    .isLength({min:3}).withMessage('Name is too short')
-    .isLength({max:20}).withMessage('Name is too long '),
+    .isLength({ min: 3 }).withMessage('Name is too short')
+    .isLength({ max: 20 }).withMessage('Name is too long'),
 
+  check('Email')
+    .notEmpty().withMessage('Email is required'),
 
-
-
-
-   check('Email')
-.   notEmpty().withMessage('Email is required'),
-
-
-
-
-
-
-
-    check('Password')
+  check('Password')
     .notEmpty().withMessage('Password is required')
-    .isLength({min:6}).withMessage('Password is too short')
-    .isLength({max:20}).withMessage('Password is too long '),
-   
-      
-    
-    
+    .isLength({ min: 6 }).withMessage('Password is too short')
+    .isLength({ max: 20 }).withMessage('Password is too long'),
 
-
-
-    check('confirmPassword')
-    .notEmpty().withMessage('ConfirmPassword is required'),
+  check('confirmPassword')
+    .notEmpty().withMessage('ConfirmPassword is required')
     .custom((value, { req }) => {
-       if (value !== req.body.Password) {
+      if (value !== req.body.Password) {
         throw new Error('Password confirmation does not match password');
       }
       return true; // Validation passed
     }),
 
-
-
-
-
-
-    check('Wight')
+  check('Wight')
     .notEmpty().withMessage('Height is required')
-    .isLength({min:1}).withMessage('Height is too short')
-    .isLength({max:3}).withMessage('Height is too long '),
+    .isLength({ min: 1 }).withMessage('Height is too short')
+    .isLength({ max: 3 }).withMessage('Height is too long'),
 
+  check('Lenght')
+    .notEmpty().withMessage('Length is required')
+    .isLength({ min: 1 }).withMessage('Length is too short')
+    .isLength({ max: 3 }).withMessage('Length is too long'),
 
+  ErrorValidation // This should be outside the array
+];
 
-    check('Lenght')
-    .notEmpty().withMessage('Lenght is required')
-    .isLength({min:1}).withMessage('Lenght is too short')
-    .isLength({max:3}).withMessage('Lenght is too long' ),
-    ErrorValidation
-    ];
+exports.LoginValidator = [
+  check('Email')
+    .notEmpty().withMessage('Email is required'),
 
+  check('Password')
+    .notEmpty().withMessage('Password is required'),
 
+  ErrorValidation // This should be outside the array
+];
 
+exports.resetPassword = [
+  check('Password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password is too short')
+    .isLength({ max: 20 }).withMessage('Password is too long'),
 
-    exports.LoginValidator = [
-
-
-
-        check('Email')
-        .notEmpty().withMessage('Email is required'),
-         check('Password')
-        .notEmpty().withMessage('Password is required'),
-
-
-        ErrorValidation
-
-    ]
-    exports.resetPassword = [
-
-        check('Password')
-        .notEmpty().withMessage('Password is required')
-        .isLength({min:6}).withMessage('Password is too short')
-        .isLength({max:20}).withMessage('Password is too long '),
-        ErrorValidation
-    ]
+  ErrorValidation // This should be outside the array
+];
