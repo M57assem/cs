429670fe9fd6a3b3163fb54b51db5efd6da85e4f
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-17 22:45:22 +0300

    Update sensor.contoller.js

--- controller/sensor.contoller.js
+++ controller/sensor.contoller.js
@@ -6,48 +6,49 @@ const asyncHandler = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
 
+// to get data from ESP32
 const saveSensorData = asyncHandler(async (req, res, next) => {
-    const userId = req.userId;
-
-    // Find data by both GPS Location and userId
-    const data = await DB.findOne({ 'GPS.Location': req.body.GPS.Location, userId: userId });
-
+    const { GPS, DHT11, MAX30105 } = req.body;
+  
+    // Example criteria for finding existing data: GPS location
+    const data = await DB.findOne({ 'GPS.Location': GPS.Location });
+  
     if (data) {
-        // If data exists, update it
-        data.DHT11 = req.body.DHT11;
-        data.MAX30105 = req.body.MAX30105;
-        data.timestamp = Date.now(); // Update timestamp
-
-        const updatedData = await data.save();
-        if (updatedData) {
-            res.status(200).json(updatedData);
-        } else {
-            return next(new ApiError('Something went wrong while updating', 500));
-        }
+      // Update existing document
+      data.DHT11 = DHT11;
+      data.MAX30105 = MAX30105;
+      data.timestamp = Date.now(); // Update timestamp
+  
+      const updatedData = await data.save();
+      if (updatedData) {
+        res.status(200).json({ message: "Data updated successfully" });
+      } else {
+        return next(new ApiError("Something went wrong while updating", 500));
+      }
     } else {
-        // If data doesn't exist, create a new entry
-        const newData = new DB({
-            GPS: req.body.GPS,
-            DHT11: req.body.DHT11,
-            MAX30105: req.body.MAX30105,
-            userId: userId,
-            timestamp: Date.now() // Add timestamp for consistency
-        });
-        const savedData = await newData.save();
-
-        if (savedData) {
-            res.status(201).json({ message: "Data is saved" });
-        } else {
-            return next(new ApiError('Something went wrong while saving', 500));
-        }
+      // Create new document
+      const newData = new DB(req.body);
+      const savedData = await newData.save();
+  
+      if (savedData) {
+        res.status(201).json({ message: "Data saved successfully" });
+      } else {
+        return next(new ApiError("Something went wrong while saving", 500));
+      }
     }
-});
+  });
+  
+  
+  
+  
+
+   
 
 const getSensorDataForUser = asyncHandler(async (req, res, next) => {
-    const userId = req.params.id;
+   
 
 
-    const data = await DB.find({ userId: userId });
+    const data = await DB.find().sort({timestamp:-1}).limit(10);
     
 
     if (data && data.length > 0) {
