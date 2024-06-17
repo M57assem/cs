57da743170f8f39c3039ef41ab7326fefbe3f648
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-17 22:45:58 +0300

    Update sensor.route.js

--- routes/sensor.route.js
+++ routes/sensor.route.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authenticateUser = require('../middleware/authinticate'); // Import authenticateUser middleware
 const as = require('../controller/sensor.contoller');
 
 // Register authenticateUser middleware for all routes in this router
-router.route('/:id') // Route accepts user ID directly
+router.route('/') // Route accepts user ID directly
     .get(as.getSensorDataForUser);
 
-router.use(authenticateUser);
+
 
 router.route('/')
     .post(as.saveSensorData);
