670882d61814bf25d783c245e657e04d788358b0
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-18 14:13:45 +0300

    Update sensor.route.js

--- routes/sensor.route.js
+++ routes/sensor.route.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const as = require('../controller/sensor.contoller');
-
-// Register authenticateUser middleware for all routes in this router
-router.route('/') // Route accepts user ID directly
-    .get(as.getSensorDataForUser);
+const { repeatAPI } = require('../controller/sensor.contoller');
 
 
 
 router.route('/')
-    .post(as.saveSensorData);
+  .get(as.getSensorDataForUser)
+  .post(as.saveSensorData);
+
+// Call repeatAPI every 2 minutes
+setInterval(repeatAPI, 1 * 60 * 1000);
 
 module.exports = router;
