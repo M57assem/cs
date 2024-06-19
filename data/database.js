036a282e4be51fe7ac0d22b096ec34fc8a347804
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-19 03:44:54 +0300

    Add files via upload

---
+++ data/database.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const authschema = new mongoose.Schema({
+
+Name:{
+    type: String,
+    required:true
+
+},
+Email:{
+    type: String,
+    required:true,
+    unique:true,
+    validate: [validator.isEmail,'Sorry Email Not Valid']
+},
+Password:{
+    type: String,
+    required:true
+    
+},
+Wight :{
+    type: String,
+    required:true,
+},
+Lenght:{
+    type: String,
+    required:true
+},
+
+token:{
+    type: String,  
+},
+
+verified: {
+    type: Boolean,
+    default: false
+}, 
+
+photo: String,
+ // Assuming you'll store filename
+
+role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+},
+
+
+
+});
+module.exports = mongoose.model('smart',authschema);
