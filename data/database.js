235c43e849b6b5c8da244be2e9204cef1ace86a5
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-19 03:35:25 +0300

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,50 +1,47 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
-const authschema = new mongoose.Schema({
+const validator = require('validator');
 
-Name:{
-    type: String,
-    required:true
-
-},
-Email:{
-    type: String,
-    required:true,
-    unique:true,
-    validate: [validator.isEmail,'Sorry Email Not Valid']
-},
-Password:{
-    type: String,
-    required:true
-    
-},
-Wight :{
-    type: String,
-    required:true,
-},
-Lenght:{
-    type: String,
-    required:true
-},
-
-token:{
-    type: String,  
-},
-
-photo: String,
-    
-verified: {
-  type: Boolean,
-  default: false
-},
-
-role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-}
-
-
-
+const authSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Sorry, email is not valid'
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: String,
+        required: true
+    },
+    length: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String
+    },
+    photo: {
+        type: String // Assuming you'll store the filename
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
 });
-module.exports = mongoose.model('smart',authschema);
+
+module.exports = mongoose.model('Smart', authSchema);
