fbf9bde1884156c6956c5ad46d0bdfb87e3af6aa
M57assem <160439357+M57assem@users.noreply.github.com>
2024-06-20 07:01:38 +0300

    Update ESP32.js

--- data/ESP32.js
+++ data/ESP32.js
@@ -1,24 +1,49 @@
 const mongoose = require('mongoose');
 const authschema = new mongoose.Schema({
+
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'smart',
+//     required: true
+// },
     timestamp: { 
         type: Date, 
         default: Date.now 
       }, // Automatically sets the current date and time when a new document is created
-    GPS: {
-         type: String,
-            default: '29.982704, 31.282639' // Default GPS coordinates
-      },
-      DHT11: {
-        temperature: Number,
-        humidity: Number,
-      },
-      MAX30105: {
-    Heartrate : Number,
-    Spo2 : Number,
-  IR : Number,
-    Red : Number,
-      },
     
+      GPS: {
+        type: String,
+           default: '29.982704, 31.282639' // Default GPS coordinates
+     
+      },
+      
+    
+      Temperature:{
+          
+        type: Number,
+
+      },
+      Humidity:{
+          
+        type: Number,
+
+      },
+      HeartRate:{
+          
+        type: Number,
+
+      },
+
+      Spo2:{
+          
+        type: Number,
+
+      },
+
+
+      
+    
+  
 
 });
 module.exports = mongoose.model('SensorData',authschema);
